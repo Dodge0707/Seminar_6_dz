998e2b7e8d4da946439cad754fcedb3121203303
«Vova <vrobskiy@gmail.com>
2022-05-04 19:49:35 +0300

    add Task number 1 Please ent the commit message for your changes. Lines starting

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int [m];
+
+FillArray(array);
+WriteArray(array);
+int result = 0;
+for (int i = 0; i < m; i++)
+{
+    if (array[i] > 0)
+    {
+        result++;
+    }
+}
+
+Console.WriteLine($"Количество чисел > 0 равно " + result);
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        Console.WriteLine("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        array[i] = number;
+    }
+}
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < m; i++)
+    {
+        Console.Write(array[i] + " "); 
+    }
+    Console.WriteLine();
+}
